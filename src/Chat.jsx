cc21d87b45bdef6447ca733d4681adaaf6de58c3
AdithyaBijimon <adithyabijimon5@gmail.com>
2025-09-17 00:16:01 +0530

    Chat app

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 
 function Chat() {
 
-    const [message,setMessage] = useState('')
-    const[displayMsg,setDisplayMsg] = useState([])
+    const [message, setMessage] = useState('')
+    const [displayMsg, setDisplayMsg] = useState([])
 
-    const handleSend = ()=>{
-        
-        setDisplayMsg([message])
-        console.log([...displayMsg,message])
+    const handleSend = () => {
+        setDisplayMsg(prevMessages => [...prevMessages, message]);
+        setMessage('');
+
     }
 
 
@@ -37,22 +37,29 @@ function Chat() {
                     </div>
                 </div>
 
-                 <div className="flex justify-start">
+                <div className="flex justify-start">
                     <div className="rounded-r-lg rounded-bl-lg bg-sky-500 text-white w-auto p-2">
                         <p>how are you?</p>
                     </div>
                 </div>
-               <div className="flex justify-end">
-                    <div className="rounded-l-lg rounded-br-lg bg-violet-500 text-white w-auto p-2">
-                        <p>I am fine 😊</p>
-                    </div>
-                </div>
+                {
+                    displayMsg.length > 0 ?
+                        displayMsg?.map(msg => (
+                            <div key={msg} className="flex justify-end">
+                                <div className="rounded-l-lg rounded-br-lg bg-violet-500 text-white w-auto p-2">
+                                    <p>{[...msg]}</p>
+                                </div>
+                            </div>
+                        )) :
+                        ''
+
+                } 
 
             </div>
 
 
             <div className='flex p-4 bg-gray-100'>
-                <input onChange={(event)=>setMessage(event.target.value)} type="text" placeholder="Enter your text" className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                <input value={message} onChange={(event) => setMessage(event.target.value)} type="text" placeholder="Enter your text" className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
                 <div className='flex items-center justify-center ms-5'>
 
                     <button onClick={handleSend} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm p-2  me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><i className="fa-brands fa-telegram text-xl"></i></button>
